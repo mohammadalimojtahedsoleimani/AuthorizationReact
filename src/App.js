529741e9454cf708692bc87f1d21493e2170daea
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-20 22:30:40 +0330

    Sign Up form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import SignUp from "./Components/SignUp";
 const App = () => {
     return (
         <div>
             <h1>Hello world!</h1>
+            <h1>{<SignUp/>}</h1>
         </div>
     );
 };
