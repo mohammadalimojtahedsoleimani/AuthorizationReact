c290365e2ca6504ebb8e5023774ef8f2643def1e
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-21 00:25:31 +0330

    validate part added

---
+++ src/Components/validate.js
@@ -0,0 +1,35 @@
+const validate = ( data ) => {
+    const errors = {};
+    if ( ! data.name.trim () ) {
+        errors.name = "Username Required"
+    } else {
+        delete errors.name
+    }
+    if ( ! data.email ) {
+        errors.email = "Email required";
+    } else if ( ! /\S+@\S+\.\S+/.test ( data.email ) ) {
+        errors.email = "Email address is incorrect"
+    } else {
+        delete errors.email;
+    }
+    if ( ! data.password ) {
+        errors.password = "Password is required"
+    } else if ( data.password < 6 ) {
+        errors.password = "Password is less than 6"
+    } else {
+        delete errors.password
+    }
+    if ( ! data.confirmPassword ) {
+        errors.confirmPassword = "confirm password"
+    } else if ( data.confirmPassword !== data.password ) {
+        errors.confirmPassword = "Confirm password doesnt  match with password"
+    } else {
+        delete errors.confirmPassword
+    }
+    if ( data.isAccepted ) {
+        delete errors.isAccepted
+    } else {
+        errors.isAccepted = "Accept our regulations"
+    }
+    return errors;
+}
